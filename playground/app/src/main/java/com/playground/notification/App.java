65c64ce7ee456c0a86d82a770a98dc26f7d5f5b9
XinyueZhao <zhao@eosnewmedia.de>
2015-08-03 22:52:23 +0200

    First commit
    Add map-key to connect map. Use wizar to create basic UIs.
    Crreate flavor:live, dev.

---
+++ playground/app/src/main/java/com/playground/notification/App.java
@@ -0,0 +1,68 @@
+/*
+                   _ooOoo_
+                  o8888888o
+                  88" . "88
+                  (| -_- |)
+                  O\  =  /O
+               ____/`---'\____
+             .'  \\|     |//  `.
+            /  \\|||  :  |||//  \
+           /  _||||| -:- |||||-  \
+           |   | \\\  -  /// |   |
+           | \_|  ''\---/''  |   |
+           \  .-\__  `-`  ___/-. /
+         ___`. .'  /--.--\  `. . __
+      ."" '<  `.___\_<|>_/___.'  >'"".
+     | | :  `- \`.;`\ _ /`;.`/ - ` : | |
+     \  \ `-.   \_ __\ /__ _/   .-` /  /
+======`-.____`-.___\_____/___.-`____.-'======
+                   `=---='
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+         佛祖保佑       永无BUG
+*/
+//          佛曰:
+//                  写字楼里写字间，写字间里程序员；
+//                  程序人员写程序，又拿程序换酒钱。
+//                  酒醒只在网上坐，酒醉还来网下眠；
+//                  酒醉酒醒日复日，网上网下年复年。
+//                  但愿老死电脑间，不愿鞠躬老板前；
+//                  奔驰宝马贵者趣，公交自行程序员。
+//                  别人笑我忒疯癫，我笑自己命太贱。
+
+package com.playground.notification;
+
+import android.app.Application;
+
+
+/**
+ * The app-object of the project.
+ *
+ * @author Xinyue Zhao
+ */
+public final class App extends Application {
+	/**
+	 * Application's instance.
+	 */
+	public static App Instance;
+	{
+		Instance = this;
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+//		Stetho.initialize(Stetho.newInitializerBuilder(this).enableDumpapp(Stetho.defaultDumperPluginsProvider(this))
+//				.enableWebKitInspector(Stetho.defaultInspectorModulesProvider(this)).build());
+//
+//		BookmarkManger.createInstance(this);
+//		Properties prop = new Properties();
+//		try {
+//			prop.load(getClassLoader().getResourceAsStream("app.properties"));
+//			Bmob.initialize(this, prop.getProperty("bmob_app"));
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
+//		TaskHelper.init(getApplicationContext());
+//		DB.getInstance(this).open();
+	}
+}
